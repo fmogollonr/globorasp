4bdfa5721fba8a15b36bf3a011b5bfce79e0ff92
Felipe Mogollon <felipemogollonr@gmail.com>
2019-05-09 22:49:50 +0200

    loop gps

--- gps.py
+++ gps.py
@@ -2,33 +2,41 @@
 #https://github.com/MartijnBraam/gpsd-py3
 import gpsd
 import math
+import time
+import os
 
 def truncate(number, digits) -> float:
     stepper = pow(10.0, digits)
     return math.trunc(stepper * number) / stepper
 
+
 truncateDigits=4
-# Connect to the local gpsd
-gpsd.connect()
 
-# Connect somewhere else
-gpsd.connect(host="127.0.0.1", port=2947)
 
-# Get gps position
 try:
-	packet = gpsd.get_current()
-	latitude=packet.lat
-	lat=(truncate(latitude,truncateDigits))
-	longitude=packet.lon
-	lon=(truncate(longitude,truncateDigits))
-	time=packet.time
-	altitude=packet.alt
-	alt=(truncate(altitude,0))
-	print(str(lat)+";"+str(lon)+";"+str(time)+";"+str(alt))
+	# Connect to the local gpsd
+	gpsd.connect()
+	while True:
+		# Get gps position
+		try:
+			packet = gpsd.get_current()
+			latitude=packet.lat
+			lat=(truncate(latitude,truncateDigits))
+			longitude=packet.lon
+			lon=(truncate(longitude,truncateDigits))
+			gpstime=packet.time
+			altitude=packet.alt
+			alt=(truncate(altitude,0))
+			print(str(lat)+";"+str(lon)+";"+str(gpstime)+";"+str(alt))
+			time.sleep(10)
 
-except:
-	#No GPS
-	print("0.0;0.0;;0.0")
+		except:
+			#No GPS
+			print("No GPS")
+			print("0.0;0.0;;0.0")
+	
+except :
+    print('End')
 
 # See the inline docs for GpsResponse for the available data
 #print(packet.position())
