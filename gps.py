b6396ab358d5bed2d9d5c3a2e0715f2148404e7a
Felipe Mogollon <felipemogollonr@gmail.com>
2019-05-12 22:41:46 +0200

    write GPS log into a folder

--- gps.py
+++ gps.py
@@ -4,15 +4,17 @@ import gpsd
 import math
 import time
 import os
+from datetime import datetime
 
 def truncate(number, digits) -> float:
     stepper = pow(10.0, digits)
     return math.trunc(stepper * number) / stepper
 
-
+#home="/home/pi/balloon/"
+home="/home/felipe/Documents/radio/soft/globorasp/"
 truncateDigits=4
-
-
+#print("opening "+home+"gps.log")
+initTime=datetime.utcnow()
 try:
 	# Connect to the local gpsd
 	gpsd.connect()
@@ -27,7 +29,14 @@ try:
 			gpstime=packet.time
 			altitude=packet.alt
 			alt=(truncate(altitude,0))
-			print(str(lat)+";"+str(lon)+";"+str(gpstime)+";"+str(alt))
+			gps_pos=str(lat)+";"+str(lon)+";"+str(gpstime)+";"+str(alt)
+			initTime=datetime.utcnow()
+			formated_time = initTime.strftime("%Y%m%d_%H%M%S")
+			data=formated_time+":"+gps_pos
+			#print(data)
+			f= open(home+"gps.log","a")
+			f.write(data+"\n")
+			f.close()
 			time.sleep(10)
 
 		except:
