80df8a09ee01497bc2d7f4a5848538b505d2c9e0
Felipe Mogollon <felipemogollonr@gmail.com>
2019-05-22 23:38:28 +0200

    El envío de las imágenes con mayor o menor frecuencia viene dada por la altura a la que está el globo

--- capture.sh
+++ capture.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
-#La duración de un ciclo de hacer foto -> transmformación a sonido sstv, reproducción de sonido dura aproximadamente 62 segundos en una raspberry pi 3 B+
+#La duración de un ciclo de hacer foto -> transnformación a sonido sstv, reproducción de sonido dura aproximadamente 62 segundos en una raspberry pi 3 B+
 #intervalos en segundos
 intervalo=1
 intervalo2=120
 indicativo="EB2ELU-11"
 mision="DEEP_SPACE"
+#altitud en metros a partir de la cual se quiere pasar de enviar fotos tan rápido (intervalo1) como se pueda a mandarlas más pausada esperando intervalo2 entre cada transmisión
+altitud_ref=100
 #directorio donde se guardan las fotos
-#home="/home/pi/balloon/"
-home="/home/felipe/Documents/radio/soft/globorasp/"
+home="/home/pi/sstv/"
+#home="/home/felipe/Documents/radio/soft/globorasp/"
 gpsFile=$home"gps.log"
 
 #contador de transmisiones
 contador=0
 #numero de transmisiones con el primer intervalo
 ntrans=10
+altitudnum=0
 while true
 do
 	#echo $contador
 
 	#extraer datos del fichero de logs del GPS
 	tmpgps=`tail -n 1 $gpsFile`
-	gpsmessage=`echo $tmpgps | sed 's/: /\n/g' | grep T`
+	gpsmessage=""
+	if [[ $tmpgps == *"0.0;0.0;;0.0"* ]]
+	then
+		gpsmessage="0.0;0.0;;0.0"
+	else
+		gpsmessage=`echo $tmpgps | sed 's/: /\n/g' | grep T`
+	fi
 
 	position="."
 	altura="."
@@ -45,50 +54,55 @@ do
 		then
 			long=${longitude::-4}
 		fi
+		altitud=`echo ${altitude%.*}`
 		position=$lat"/"$long
-		altura="$altitude.m"
+		altura="$altitud.M"
 		# configuramos la hora de la raspberry desde el GPS
 		date -s "$utc_fecha"
 	fi
 
-# tomamos la hora
-fecha=$(date +"%Y-%m-%d_%H%M%S")
-#hacemos una foto y la guardamos como fecha_hora.jpg
-#raspistill -o $home"/"$fecha.big.jpg
-#random jpg
-mx=320;my=240;head -c "$((3*mx*my))" /dev/urandom | convert -depth 8 -size "${mx}x${my}" RGB:- $fecha.big.jpg
-#redimensionamos la foto a 320x240
-convert $home"/"$fecha.big.jpg -resize 320x240! $home"/"$fecha.jpg
-#Insertamos el nombre de la misión arriba a la izquierda en rojo
-convert -pointsize 20 -fill red  -gravity NorthWest -annotate 0 $mision $home"/"$fecha.jpg $home"/"$fecha.1.jpg
-# insertamos el indicativo abajo a la derecha en rojo
-convert -pointsize 20 -fill red -gravity SouthEast -annotate 0 $indicativo $home"/"$fecha.1.jpg $home"/"$fecha.2.jpg
-#Insertamos la altitud abajo a la izquierda
-convert -pointsize 20 -fill red -gravity SouthWest -annotate 0 $altura $home"/"$fecha.2.jpg $home"/"$fecha.3.jpg
-#Insertamos la posición arriba a la derecha
-convert -pointsize 20 -fill red -gravity NorthEast -annotate 0 $position $home"/"$fecha.3.jpg $home"/"$fecha.jpg
+	# tomamos la hora
+	fecha=$(date +"%Y-%m-%d_%H%M%S")
+	#hacemos una foto y la guardamos como fecha_hora.jpg
+	raspistill -o $home"/"$fecha.big.jpg
+	# imágen en blanco
+	#convert -size 320x240 xc:white $fecha.big.jpg
+	#redimensionamos la foto a 320x240
+	convert $home"/"$fecha.big.jpg -resize 320x240! $home"/"$fecha.jpg
 
-#convertimos la foto a a un wav que contiene el sstv
-python3 -m pysstv --mode Robot36 --vox $home"/"$fecha.jpg $home"/"$fecha.sstv.wav
-# reproducimos el wav
-aplay $home"/"$fecha.sstv.wav
-#borramos foto temporal
-rm $home"/"$fecha.1.jpg
-rm $home"/"$fecha.2.jpg
-rm $home"/"$fecha.3.jpg
-#borramos el wav
-rm $home"/"$fecha.sstv.wav
-# comprobamos si llevamos menos transmisiones que laqs que queremos para el primer intervalo
+	#imágen aleatoria jpg
+	#mx=320;my=240;head -c "$((3*mx*my))" /dev/urandom | convert -depth 8 -size "${mx}x${my}" RGB:- $fecha.jpg
 
-echo "DONE"
-if [ $contador -lt $ntrans ]
-then
-	#si llevamos menos, paramos lun tiempo que dura el primer intervalo
-	sleep $intervalo
-else
-	# Si ya hemos hecho más transmisiones paramos el segundo intervalo
-	sleep $intervalo2
-fi
-#actualizamos el contador de transmisiones
-contador=`expr $contador + 1`
+	#Insertamos el nombre de la misión arriba a la izquierda en rojo
+	convert -pointsize 20 -fill red  -gravity NorthWest -annotate 0 $mision $home"/"$fecha.jpg $home"/"$fecha.1.jpg
+	# insertamos el indicativo abajo a la derecha en rojo
+	convert -pointsize 20 -fill red -gravity SouthEast -annotate 0 $indicativo $home"/"$fecha.1.jpg $home"/"$fecha.2.jpg
+	#Insertamos la altitud abajo a la izquierda
+	convert -pointsize 20 -fill red -gravity SouthWest -annotate 0 $altura $home"/"$fecha.2.jpg $home"/"$fecha.3.jpg
+	#Insertamos la posición arriba a la derecha
+	convert -pointsize 20 -fill red -gravity NorthEast -annotate 0 $position $home"/"$fecha.3.jpg $home"/"$fecha.jpg
+	
+	#convertimos la foto a a un wav que contiene el sstv
+	python3 -m pysstv --mode Robot36 --vox $home"/"$fecha.jpg $home"/"$fecha.sstv.wav
+	# reproducimos el wav
+	aplay $home"/"$fecha.sstv.wav
+	#borramos foto temporal
+	rm $home"/"$fecha.1.jpg
+	rm $home"/"$fecha.2.jpg
+	rm $home"/"$fecha.3.jpg
+	#borramos el wav
+	rm $home"/"$fecha.sstv.wav
+	# comprobamos si llevamos menos transmisiones que laqs que queremos para el primer intervalo
+	echo "DONE"
+	if [ $((altitud)) -lt $altitud_ref ]
+	then
+		#si llevamos menos, paramos lun tiempo que dura el primer intervalo
+		sleep $intervalo
+	else
+		# Si ya hemos hecho más transmisiones paramos el segundo intervalo
+		sleep $intervalo2
+	fi
+	#actualizamos el contador de transmisiones
+	contador=`expr $contador + 1`
 done
+	
