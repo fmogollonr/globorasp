7092e7646081b0bca8d805e31120256ad58128a6
Felipe Mogollon <felipemogollonr@gmail.com>
2019-05-22 23:39:07 +0200

    cliente gps más resistente a errores

--- gps.py
+++ gps.py
@@ -10,42 +10,57 @@ def truncate(number, digits) -> float:
     stepper = pow(10.0, digits)
     return math.trunc(stepper * number) / stepper
 
-#home="/home/pi/balloon/"
-home="/home/felipe/Documents/radio/soft/globorasp/"
+home="/home/pi/sstv/"
+#home="/home/felipe/Documents/radio/soft/globorasp/"
 truncateDigits=4
 #print("opening "+home+"gps.log")
 initTime=datetime.utcnow()
-try:
-	# Connect to the local gpsd
-	gpsd.connect()
-	while True:
-		# Get gps position
+while True:
+	try:
+		# Connect to the local gpsd
+		print("Trying to connect")
 		try:
-			packet = gpsd.get_current()
-			latitude=packet.lat
-			lat=(truncate(latitude,truncateDigits))
-			longitude=packet.lon
-			lon=(truncate(longitude,truncateDigits))
-			gpstime=packet.time
-			altitude=packet.alt
-			alt=(truncate(altitude,0))
-			gps_pos=str(lat)+";"+str(lon)+";"+str(gpstime)+";"+str(alt)
-			initTime=datetime.utcnow()
-			formated_time = initTime.strftime("%Y%m%d_%H%M%S")
-			data=formated_time+": "+gps_pos
-			#print(data)
-			f= open(home+"gps.log","a")
-			f.write(data+"\n")
-			f.close()
-			time.sleep(10)
+			gpsd.connect()
+			while True:
+				# Get gps position
+				try:
+					packet = gpsd.get_current()
+					latitude=packet.lat
+					lat=(truncate(latitude,truncateDigits))
+					longitude=packet.lon
+					lon=(truncate(longitude,truncateDigits))
+					gpstime=packet.time
+					altitude=packet.alt
+					alt=(truncate(altitude,0))
+					gps_pos=str(lat)+";"+str(lon)+";"+str(gpstime)+";"+str(alt)
+					initTime=datetime.utcnow()
+					formated_time = initTime.strftime("%Y%m%d_%H%M%S")
+					data=formated_time+": "+gps_pos
+					f= open(home+"gps.log","a")
+					f.write(data+"\n")
+					f.close()
+					time.sleep(10)
 
+				except:
+					#No GPS
+					print("No GPS")
+					print("0.0;0.0;;0.0")
+					initTime=datetime.utcnow()
+					formated_time = initTime.strftime("%Y%m%d_%H%M%S")
+					gps_pos="0.0;0.0;;0.0"
+					data=formated_time+": "+gps_pos
+					f= open(home+"gps.log","a")
+					f.write(data+"\n")
+					f.close()
+					time.sleep(10)
+					gpsd.connect()
+
+					#exit()
 		except:
-			#No GPS
-			print("No GPS")
-			print("0.0;0.0;;0.0")
+			print("cannot connect")
 	
-except :
-    print('End')
+	except :
+		print('End')
 
 # See the inline docs for GpsResponse for the available data
 #print(packet.position())
