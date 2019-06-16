9b431098364984eb973adbf08a77778169270f00
fmogollon <felipemogollonr@gmail.com>
2019-06-16 12:08:21 +0200

    read from gps log file

--- aprstx.py
+++ aprstx.py
@@ -10,17 +10,34 @@ def zpad(val, n):
         return "%s.%s" % (bits[0].zfill(n+1), bits[1])
     return "%s.%s" % (bits[0].zfill(n), bits[1])
 
+def getGPSLine(filePath):
+    fileHandle = open ( filePath,"r" )
+    lineList = fileHandle.readlines()
+    fileHandle.close()
+    #print("The last line is:")
+    # or simply
+    #print(lineList[-1])
+    return lineList[-1]
+
+home_folder="/home/pi/sstv/"
+gps_file=home_folder+"gps.log"
+gps_message=getGPSLine(gps_file)
+message_tmp=gps_message.split(":")[1]
+gps_parts=message_tmp.split(";")
+print(gps_parts)
+lon=gps_parts[0]
+lat=gps_parts[1]
+hour=gps_parts[2]
+alt=message_tmp[1]
 
 callsign="EB2ELU-11"
 outputfile="/tmp/aprs.wav"
 time=datetime.datetime.utcnow().strftime("%H:%M:%S").split('.')
-lat="43.3235"
-lon="-1.9678"
-speed="150" #m/s
-temp="-10" #grados centígrados
-alt="1000" #feet
+#speed="150" #m/s
+#temp="-10" #grados centígrados
+#alt="1000" #feet
 msg="testing"
-pre="1000.0"
+#pre="1000.0"
 
 auxLat=float(lat)*100
 auxLong=float(lon)*100
@@ -36,7 +53,8 @@ except:
 
 #http://wiki.ashab.space/doku.php?id=ns1:telemetria
 #EA1IDZ-11>WORLD,WIDE2-2:!4331.52N/00540.05WO0/0.020/A=37.2/V=7.64/P=1018.0/TI=29.50/TO=26.94/23-04-2016/19:52:49/GPS=43.525415N,005.667503W/EA1IDZ test baliza APRS/SSTV ea1idz@ladecadence.net
-message=callsign+">WORLD,WIDE2-2:!"+newLat+"N/"+newLong+"E/"+speed+"/A="+alt+"/TO="+temp+"/"+str(time[0])+"/"+msg
+message=callsign+">WORLD,WIDE2-2:!"+newLat+"N/"+newLong+"E/A="+alt+"/"+str(time[0])+"/"+msg
+print(message)
 command="echo -n \""+message+"\" | gen_packets -a 100 -o "+outputfile+" - "
-os.system(command)
-os.system("aplay "+outputfile)
+#os.system(command)
+#os.system("aplay "+outputfile)
