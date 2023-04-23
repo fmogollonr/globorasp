2ce04195ceafbe4f500be4917d472d32ad8f3c76
fmogollonr <felipemogollonr@gmail.com>
2023-04-23 22:16:32 +0200

    Raspberry pi 4 raspbian 11

--- capture.sh
+++ capture.sh
@@ -21,72 +21,80 @@ altitud_ref=100
 #NO TOCAR
 #directorio donde se guardan las fotos
 home="/home/pi/sstv/"
+mkdir -p $home
 #home="/home/felipe/Documents/radio/soft/globorasp/"
 gpsFile=$home"gps.log"
 
 #contador de transmisiones
 contador=0
 
+set -x
 altitud=0
 while true
 do
-	#echo $contador
-
+	echo $contador
+	gpspipe -w -n 10 |   grep -m 1 lon | python -mjson.tool > /tmp/gps.pos ; lat=`cat /tmp/gps.pos | grep lat | sed 's/,//g' | sed 's/"lat": //g' | sed 's/ //g'` ; lon=`cat /tmp/gps.pos | grep lon | sed 's/,//g' | sed 's/"lon": //g' | sed 's/ //g' ` ; alt=`cat /tmp/gps.pos | grep altMSL | sed 's/,//g' | sed 's/"altMSL": //g' | sed 's/ //g'` 
+	
 	#extraer datos del fichero de logs del GPS, se lee la última línea
-	tmpgps=`tail -n 1 $gpsFile`
-	gpsmessage=""
-	if [[ $tmpgps == *"0.0;0.0;;0.0"* ]]
-	then
-		gpsmessage="0.0;0.0;;0.0"
-	else
-		gpsmessage=`echo $tmpgps | sed 's/: /\n/g' | grep T`
-	fi
+	#tmpgps=`tail -n 1 $gpsFile`
+	#gpsmessage=""
+	#if [[ $tmpgps == *"0.0;0.0;;0.0"* ]]
+	#then
+#		gpsmessage="0.0;0.0;;0.0"
+#	else
+#		gpsmessage=`echo $tmpgps | sed 's/: /\n/g' | grep T`
+#	fi
 
-	position="."
-	altura="."
+	position=""
+	altura=$alt
+	altitud=$alt
+	lati=${lat::5}
+	long=${lon::5}
+	#lat_len=$(echo -n $lat | wc -m)
+	position=$lati"/"$long
 	#Si la salida del GPS es correcta se parsean los datos
-	if [[ $gpsmessage != "0.0;0.0;;0.0" ]]
-	then
+	#if [[ $gpsmessage != "0.0;0.0;;0.0" ]]
+	#then
 		# Separo por punto y coma
 		#horaGPS;Lat;LatOrientación;Lon;LonOrientación;altitud en metros;velocidad
-		IFS=';' read -ra ADDR <<< "$gpsmessage"
-		utc_fecha=${ADDR[0]}
-		latitude=${ADDR[1]}
-		latO=${ADDR[2]}
-		longitude=${ADDR[3]}
-		lonO=${ADDR[4]}
-		altitude=${ADDR[5]}
-		speed=${ADDR[6]}
+		#IFS=';' read -ra ADDR <<< "$gpsmessage"
+		#utc_fecha=${ADDR[0]}
+		#latitude=${ADDR[1]}
+		#latO=${ADDR[2]}
+		#longitude=${ADDR[3]}
+		#lonO=${ADDR[4]}
+		#altitude=${ADDR[5]}
+		#speed=${ADDR[6]}
 
 
-		tmpLat=$(echo $latitude | sed 's/^0*//')
-		tmpLon=$(echo $longitude | sed 's/^0*//')
+		#tmpLat=$(echo $latitude | sed 's/^0*//')
+		#tmpLon=$(echo $longitude | sed 's/^0*//')
 
 
 		#echo $tmpLon
 		# Si latitud o longitud tienen un tamaño muy grande se recortan para que entren bien en la imágen
-		lat=`echo "print($tmpLat/100)" | python3`
-		lon=`echo "print($tmpLon/100)" | python3`
-		lat_len=$(echo -n $lat | wc -m)
-		lon_len=$(echo -n $lon | wc -m)
-		latRest=`echo "print($lat_len - 6)" | python3`
-		lonRest=`echo "print($lon_len - 5)" | python3`
-		lat=${lat::-$latRest}
-		lon=${lon::-$lonRest}
-		altitud=`echo ${altitude%.*}`
-		position=$lat$latO"/"$lon$lonO
-		altura="$altitud.M"
+		#lat=`echo "print($tmpLat/100)" | python3`
+		#lon=`echo "print($tmpLon/100)" | python3`
+		#lat_len=$(echo -n $lat | wc -m)
+		#lon_len=$(echo -n $lon | wc -m)
+		#latRest=`echo "print($lat_len - 6)" | python3`
+		#lonRest=`echo "print($lon_len - 5)" | python3`
+		#lat=${lat::-$latRest}
+		#lon=${lon::-$lonRest}
+		#altitud=`echo ${altitude%.*}`
+		#position=$lat$latO"/"$lon$lonO
+		#altura="$altitud.M"
 		# configuramos la hora de la raspberry desde el GPS
-		date -s "$utc_fecha"
-	fi
+		#date -s "$utc_fecha"
+	#fi
 
 	# tomamos la hora
 	fecha=$(date +"%Y-%m-%d_%H%M%S")
 	#hacemos una foto y la guardamos como fecha_hora.jpg
-	raspistill -o $home"/"$fecha.big.jpg
+	libcamera-jpeg -t 1 --width 2592  --height 1944  -o $home"/"$fecha.big.jpg
+	# raspistill -o $home"/"$fecha.big.jpg
 	# imágen en blanco
 	#convert -size 320x240 xc:white $fecha.big.jpg
-
 	#redimensionamos la foto a 320x240
 	convert $home"/"$fecha.big.jpg -resize 320x240! $home"/"$fecha.jpg
 
